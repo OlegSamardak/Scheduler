8fa415b0c46a018cd406769dabb831585cf157fc
OlegSamardak <os1998os@ukr.net>
2018-01-25 11:07:28 +0200

    google authorization. cannot get credentials.

--- src/main/java/com/interlink/authorization/controller/GoogleAuthorizationController.java
+++ src/main/java/com/interlink/authorization/controller/GoogleAuthorizationController.java
@@ -4,7 +4,9 @@ import com.auth0.AuthenticationController;
 import com.auth0.IdentityVerificationException;
 import com.auth0.SessionUtils;
 import com.auth0.Tokens;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -16,50 +18,42 @@ import com.google.api.services.calendar.Calendar;
 import com.interlink.authorization.service.GoogleAuthorizationService;
 import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.HeaderTokenizer;
 import jdk.nashorn.internal.parser.Token;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mortbay.util.ajax.JSONDateConvertor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.filter.CompositeFilter;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.ArrayList;
 
 @RestController
-public class GoogleAuthorizationController {
-
-    GoogleAuthorizationService authorizationService;
-    AuthenticationController controller;
-
+public class GoogleAuthorizationController implements Serializable{
     @Autowired
-    public GoogleAuthorizationController(GoogleAuthorizationService authorizationService) {
-        this.authorizationService = authorizationService;
-        this.controller = AuthenticationController.newBuilder(
-                "university-scheduler.eu.auth0.com",
-                "d55nVilcpllMHHFt_tgnlP8e7X8cu5Z-",
-                "CRT9Kxk3GOB82L8jRdNYYBGq9Y7sYSSIBDLDyklsLQ5eFlUcf7UIwak6fs2r0jPf"
-        )
-                .build();
+    private GoogleAuthorizationService authorizationService;
+
+    @RequestMapping(value = "/login/google", method = RequestMethod.GET)
+    public RedirectView googleConnectionStatus(HttpServletRequest request) throws Exception {
+        return new RedirectView(authorizationService.authorize());
     }
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public void authorize(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, IdentityVerificationException {
+    @RequestMapping(value = "/login/google", method = RequestMethod.GET, params = "code")
+    public @ResponseBody Credential oauth2Callback(@RequestParam(value = "code") String code) {
+        Credential credential = authorizationService.oauth2Callback(code);
 
-        String authorizeUrl
-                = controller.buildAuthorizeUrl(request, "http://localhost:8080/abc")
-                .build();
-
-
-        response.sendRedirect(authorizeUrl);
+        return credential;
     }
 
-    @RequestMapping(path = "/abc", method = RequestMethod.GET)
-    public String abc(HttpServletRequest request, HttpServletResponse response) throws IdentityVerificationException, IOException {
-        Tokens tokens = controller.handle(request);
-        SessionUtils.set(request, "token", tokens.getAccessToken());
-
-        return "Token :" + request.getSession(false).getAttribute("token");
-    }
 }
--- src/main/java/com/interlink/authorization/service/GoogleAuthorizationService.java
+++ src/main/java/com/interlink/authorization/service/GoogleAuthorizationService.java
@@ -1,64 +1,62 @@
 package com.interlink.authorization.service;
 
 
+import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.CalendarScopes;
-import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties;
+import com.google.api.services.calendar.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class GoogleAuthorizationService {
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-            System.getProperty("user.home"), ".credentials/");
-
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
+    private final static Log LOGGER = LogFactory.getLog(GoogleAuthorizationService.class);
     private static HttpTransport HTTP_TRANSPORT;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private GoogleClientSecrets clientSecrets;
+    private GoogleAuthorizationCodeFlow flow;
 
-    private final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
+    @Value("${google.client.client-id}")
+    private String clientId;
+    @Value("${google.client.client-secret}")
+    private String clientSecret;
+    @Value("${google.client.redirectUri}")
+    private String redirectURI;
 
-    static {
-        try {
+    public String authorize() throws Exception {
+        AuthorizationCodeRequestUrl authorizationUrl;
+        if (flow == null) {
+            GoogleClientSecrets.Details web = new GoogleClientSecrets.Details();
+            web.setClientId(clientId);
+            web.setClientSecret(clientSecret);
+            clientSecrets = new GoogleClientSecrets().setWeb(web);
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
+            flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets,
+                    Collections.singleton(CalendarScopes.CALENDAR)).build();
         }
+        authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectURI);
+        LOGGER.warn(authorizationUrl.build());
+        return authorizationUrl.build();
     }
 
-    public Credential authorize() throws IOException {
-        InputStream in =
-                GoogleAuthorizationService.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                        .setDataStoreFactory(DATA_STORE_FACTORY)
-                        .setAccessType("offline")
-                        .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver()).authorize("user");
-
-        return credential;
+    public Credential oauth2Callback(String code) {
+        Credential credential;
+        try {
+            TokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectURI).execute();
+            credential = flow.createAndStoreCredential(response, "userID");
+            return credential;
+        } catch (Exception e) {
+            LOGGER.warn("Exception while handling OAuth2 callback (" + e.getMessage() + ").");
+        }
+        return null;
     }
 }
