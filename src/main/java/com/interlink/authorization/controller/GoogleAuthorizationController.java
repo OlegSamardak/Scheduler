d4a2d68c797f598e647044d95bc9506cebe36fda
EvgenySytnyk <sytnyk.evgeny@gmail.com>
2018-01-25 12:57:43 +0200

    Added saving of credential into the session after authorization

--- src/main/java/com/interlink/authorization/controller/GoogleAuthorizationController.java
+++ src/main/java/com/interlink/authorization/controller/GoogleAuthorizationController.java
@@ -1,46 +1,23 @@
 package com.interlink.authorization.controller;
 
-import com.auth0.AuthenticationController;
-import com.auth0.IdentityVerificationException;
-import com.auth0.SessionUtils;
-import com.auth0.Tokens;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.apache.ApacheHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.calendar.Calendar;
 import com.interlink.authorization.service.GoogleAuthorizationService;
-import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.HeaderTokenizer;
-import jdk.nashorn.internal.parser.Token;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.mortbay.util.ajax.JSONDateConvertor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.context.ApplicationContext;
+import com.google.api.services.calendar.Calendar;
+
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.filter.CompositeFilter;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Serializable;
-import java.security.Principal;
-import java.util.ArrayList;
 
 @RestController
-public class GoogleAuthorizationController implements Serializable{
+public class GoogleAuthorizationController implements Serializable {
+
     @Autowired
     private GoogleAuthorizationService authorizationService;
 
@@ -50,10 +27,10 @@ public class GoogleAuthorizationController implements Serializable{
     }
 
     @RequestMapping(value = "/login/google", method = RequestMethod.GET, params = "code")
-    public @ResponseBody Credential oauth2Callback(@RequestParam(value = "code") String code) {
+    public @ResponseBody
+    void oauth2Callback(@RequestParam(value = "code") String code, HttpServletRequest request) throws IOException {
         Credential credential = authorizationService.oauth2Callback(code);
-
-        return credential;
+        HttpSession session = request.getSession();
+        session.setAttribute("credential", credential);
     }
-
 }
