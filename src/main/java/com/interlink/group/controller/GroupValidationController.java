6e1aa105edf022de1f74dcebd202f08ce23eb6c8
OlegSamardak <os1998os@ukr.net>
2018-01-25 16:00:18 +0200

    REST controller updated.

--- src/main/java/com/interlink/group/controller/GroupValidationController.java
+++ src/main/java/com/interlink/group/controller/GroupValidationController.java
@@ -3,10 +3,7 @@ package com.interlink.group.controller;
 import com.interlink.entity.Group;
 import com.interlink.group.service.GroupValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GroupValidationController {
@@ -14,8 +11,10 @@ public class GroupValidationController {
     private GroupValidationService groupValidationService;
 
     @CrossOrigin
-    @GetMapping(path = "/groups")
-    public boolean isGroupExists(@RequestParam Group group){
+    @GetMapping(path = "/groups/{name}")
+    public boolean isGroupExists(@PathVariable String name){
+        Group group = new Group();
+        group.setName(name);
         return groupValidationService.checkGroupExistence(group);
     }
 }
